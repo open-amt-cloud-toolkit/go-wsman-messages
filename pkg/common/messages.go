3079f0c3d1c61752f6960672aa9dd34c12d3dc33
Tim Shockley <timothy.shockley@intel.com>
2023-08-25 14:12:56 -0700

    fix: removed commented code

--- pkg/common/messages.go
+++ pkg/common/messages.go
@@ -5,20 +5,6 @@ import (
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/wsman"
 )
 
-//type EnumerationResponse struct {
-//	XMLName xml.Name        `xml:"Envelope"`
-//	Header  wsman.Header    `xml:"Header"`
-//	Body    EnumerationBody `xml:"Body"`
-//}
-//
-//type EnumerationBody struct {
-//	EnumerateResponse EnumerateResponse `xml:"EnumerateResponse"`
-//}
-//
-//type EnumerateResponse struct {
-//	EnumerationContext string `xml:"EnumerationContext"`
-//}
-
 type EnumerationResponse struct {
 	XMLName xml.Name `xml:"Envelope"`
 	Header  wsman.Header
