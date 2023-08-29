3f2a7a508b322d224beb01c1ada8578670fedfba
Tim Shockley <timothy.shockley@intel.com>
2023-08-29 11:54:50 -0700

    feat: credential.context and concrete.dependency support

---
+++ pkg/cim/credential/context.go
@@ -0,0 +1,60 @@
+/*********************************************************************
+ * Copyright (c) Intel Corporation 2023
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
+
+package credential
+
+import (
+	"encoding/xml"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/wsman"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/cim/models"
+)
+
+type ContextPullResponseEnvelope struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Header  wsman.Header
+	Body    ContextPullResponseBody
+}
+
+type ContextPullResponseBody struct {
+	PullResponse ContextResponse
+}
+
+type ContextResponse struct {
+	EnumerationContext string
+	Items              []Relationship `xml:"Items>CIM_CredentialContext"`
+}
+
+type Relationship struct {
+	ElementInContext        models.AssociationReference
+	ElementProvidingContext models.AssociationReference
+}
+
+type Context struct {
+	base wsman.Base
+}
+
+const ClassName = "CIM_CredentialContext"
+
+// NewContext returns a new instance of the NewContext struct.
+func NewContext(wsmanMessageCreator *wsman.WSManMessageCreator) Context {
+	return Context{
+		base: wsman.NewBase(wsmanMessageCreator, ClassName),
+	}
+}
+
+// Get the representation of the instance
+func (b Context) Get() string {
+	return b.base.Get(nil)
+}
+
+// Enumerate the instances of this class
+func (b Context) Enumerate() string {
+	return b.base.Enumerate()
+}
+
+// Pull instances of this class, following an Enumerate operation
+func (b Context) Pull(enumerationContext string) string {
+	return b.base.Pull(enumerationContext)
+}
---
+++ pkg/cim/credential/context_test.go
@@ -0,0 +1,48 @@
+/*********************************************************************
+ * Copyright (c) Intel Corporation 2023
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
+
+package credential
+
+import (
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/wsman"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsmantesting"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCIM(t *testing.T) {
+	messageID := 0
+	resourceUriBase := "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/"
+	wsmanMessageCreator := wsman.NewWSManMessageCreator(resourceUriBase)
+	elementUnderTest := NewContext(wsmanMessageCreator)
+
+	t.Run("cim_* Tests", func(t *testing.T) {
+		tests := []struct {
+			name         string
+			method       string
+			action       string
+			body         string
+			responseFunc func() string
+		}{
+			//GETS
+			{"should create a valid cim_IEEE8021xSettings Get wsman message", ClassName, wsmantesting.GET, "", elementUnderTest.Get},
+			//ENUMERATES
+			{"should create a valid cim_IEEE8021xSettings Enumerate wsman message", ClassName, wsmantesting.ENUMERATE, wsmantesting.ENUMERATE_BODY, elementUnderTest.Enumerate},
+			//PULLS
+			{"should create a valid cim_IEEE8021xSettings Pull wsman message", ClassName, wsmantesting.PULL, wsmantesting.PULL_BODY, func() string { return elementUnderTest.Pull(wsmantesting.EnumerationContext) }},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				correctResponse := wsmantesting.ExpectedResponse(messageID, resourceUriBase, test.method, test.action, "", test.body)
+				messageID++
+				response := test.responseFunc()
+				if response != correctResponse {
+					assert.Equal(t, correctResponse, response)
+				}
+			})
+		}
+	})
+}
