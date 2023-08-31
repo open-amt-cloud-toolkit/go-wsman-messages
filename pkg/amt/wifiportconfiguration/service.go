be551ce2b8fe5394012910ff06009db8a5506b89
Mike Johanson <michael.johanson@intel.com>
2023-08-31 10:44:41 -0700

    feat(amt): add amt.wifiportconfiguration.AddWiFiSettingsResponse

--- pkg/amt/wifiportconfiguration/service.go
+++ pkg/amt/wifiportconfiguration/service.go
@@ -7,6 +7,7 @@ package wifiportconfiguration
 
 import (
 	"encoding/xml"
+	"strconv"
 
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/wsman"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/amt/actions"
@@ -15,21 +16,20 @@ import (
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/cim/wifi"
 )
 
-type Response struct {
+type PortConfigurationResponse struct {
 	XMLName xml.Name     `xml:"Envelope"`
 	Header  wsman.Header `xml:"Header"`
-	Body    Body         `xml:"Body"`
+	Body    PortConfigurationBody
 }
 
-type Body struct {
-	XMLName                      xml.Name                     `xml:"Body"`
-	WiFiPortConfigurationService WiFiPortConfigurationService `xml:"AMT_WiFiPortConfigurationService"`
+type PortConfigurationBody struct {
+	XMLName                      xml.Name `xml:"Body"`
+	WiFiPortConfigurationService WiFiPortConfigurationService
 }
 
 const AMT_WiFiPortConfigurationService = "AMT_WiFiPortConfigurationService"
 
 type WiFiPortConfigurationService struct {
-	models.NetworkPortConfigurationService
 	XMLName                            xml.Name `xml:"AMT_WiFiPortConfigurationService"`
 	RequestedState                     RequestedState
 	EnabledState                       EnabledState
@@ -39,7 +39,7 @@ type WiFiPortConfigurationService struct {
 	SystemName                         string
 	CreationClassName                  string
 	Name                               string
-	LocalProfileSynchronizationEnabled LocalProfileSynchronizationEnabled
+	LocalProfileSynchronizationEnabled LocalProfileSynchronizationEnabled `xml:"localProfileSynchronizationEnabled"`
 	LastConnectedSsidUnderMeControl    string
 	NoHostCsmeSoftwarePolicy           NoHostCsmeSoftwarePolicy
 	UEFIWiFiProfileShareEnabled        UEFIWiFiProfileShareEnabled
@@ -107,9 +107,26 @@ const (
 
 type UEFIWiFiProfileShareEnabled int
 
+func (t *UEFIWiFiProfileShareEnabled) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var strVal string
+	err := d.DecodeElement(&strVal, &start)
+	if err != nil {
+		return err
+	}
+	enabled, _ := strconv.ParseBool(strVal)
+	if enabled {
+		//*t = true
+		*t = (UEFIWiFiProfileShareEnabled)(1)
+	} else {
+		//*t = false
+		*t = (UEFIWiFiProfileShareEnabled)(0)
+	}
+	return nil
+}
+
 const (
-	Enabled UEFIWiFiProfileShareEnabled = iota
-	Disabled
+	Disabled UEFIWiFiProfileShareEnabled = iota
+	Enabled
 )
 
 type Service struct {
@@ -163,6 +180,7 @@ func (s Service) Pull(enumerationContext string) string {
 
 // Put will change properties of the selected instance
 func (s Service) Put(wiFiPortConfigurationService WiFiPortConfigurationService) string {
+	//wiFiPortConfigurationService.XMLSchema = "http://intel.com/wbem/wscim/1/amt-schema/1/AMT_WiFiPortConfigurationService"
 	return s.base.Put(wiFiPortConfigurationService, false, nil)
 }
 
@@ -223,4 +241,25 @@ func (s Service) AddWiFiSettings(wifiEndpointSettings models.WiFiEndpointSetting
 
 	body := s.base.WSManMessageCreator.CreateBody(string(methods.AddWiFiSettings)+"_INPUT", AMT_WiFiPortConfigurationService, &input)
 	return s.base.WSManMessageCreator.CreateXML(header, body)
+}
+
+type AddWiFiSettingsResponse struct {
+	XMLName xml.Name            `xml:"Envelope"`
+	Header  wsman.Header        `xml:"Header"`
+	Body    AddWiFiSettingsBody `xml:"Body"`
+}
+
+type AddWiFiSettingsBody struct {
+	XMLName                xml.Name               `xml:"Body"`
+	AddWiFiSettings_OUTPUT AddWiFiSettings_OUTPUT `xml:"AddWiFiSettings_OUTPUT"`
+}
+
+type AddWiFiSettings_OUTPUT struct {
+	XMLName              xml.Name `xml:"g:AddWiFiSettings_OUTPUT"`
+	G                    string   `xml:"xmlns:g,attr"`
+	WiFiEndpointSettings models.WiFiEndpointSettings
+	IEEE8021xSettings    *models.IEEE8021xSettings `xml:"g:IEEE8021xSettingsInput,omitempty"`
+	ClientCredential     *ClientCredential         `xml:"g:ClientCredential,omitempty"`
+	CACredential         *CACredential             `xml:"g:CACredential,omitempty"`
+	ReturnValue          int                       `xml:"g:ReturnValue"`
 }
