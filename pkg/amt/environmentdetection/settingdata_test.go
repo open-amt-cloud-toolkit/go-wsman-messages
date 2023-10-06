ff0490a647ad7b4e233a20127969c5b7c9a8b569
Zaid <zaid.usmani@intel.com>
2023-10-06 10:25:13 -0700

    feat(amt): adds execute support for environment

--- pkg/amt/environmentdetection/settingdata_test.go
+++ pkg/amt/environmentdetection/settingdata_test.go
@@ -6,11 +6,13 @@
 package environmentdetection
 
 import (
+	"encoding/xml"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/common"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/message"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsman"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsmantesting"
 )
 
@@ -18,32 +20,68 @@ func TestAMT_EnvironmentDetectionSettingData(t *testing.T) {
 	messageID := 0
 	resourceUriBase := "http://intel.com/wbem/wscim/1/amt-schema/1/"
 	wsmanMessageCreator := message.NewWSManMessageCreator(resourceUriBase)
-	elementUnderTest := NewEnvironmentDetectionSettingData(wsmanMessageCreator)
-
+	//client := MockClient{} // wsman.NewClient("http://localhost:16992/wsman", "admin", "P@ssw0rd", true)
+	//elementUnderTest := NewServiceWithClient(wsmanMessageCreator, &client)
+	// enumerationId := ""
+	client := wsman.NewClient("http://localhost:16992/wsman", "admin", "Intel123!", true)
+	elementUnderTest := NewEnvironmentDetectionSettingDataWithClient(wsmanMessageCreator, client)
 	t.Run("amt_* Tests", func(t *testing.T) {
 		tests := []struct {
-			name         string
-			method       string
-			action       string
-			body         string
-			responseFunc func() string
+			name         		string
+			method       		string
+			action       		string
+			body         		string
+			responseFunc 	 	func() (Response, error)
+			expectedResponse 	interface{}
 		}{
 			//GETS
-			{"should create a valid AMT_EnvironmentDetectionSettingData Get wsman message", "AMT_EnvironmentDetectionSettingData", wsmantesting.GET, "", elementUnderTest.Get},
+			{
+				"should create a valid AMT_EnvironmentDetectionSettingData Get wsman message", 
+				"AMT_EnvironmentDetectionSettingData", 
+				wsmantesting.GET, 
+				"", 
+				func() (Response, error) {
+					//client.CurrentMessage = "Get"
+					return elementUnderTest.Get()
+				},
+				Body{
+					XMLName: xml.Name{Space: "http://www.w3.org/2003/05/soap-envelope", Local: "Body"},
+					DetectionSettingData: DetectionSettingData{
+						DetectionStrings: "9fda2ad1-2d48-4853-8bd7-b09fdaf899de.com",
+						ElementName: "Intel(r) AMT Environment Detection Settings",
+						InstanceID:  "Intel(r) AMT Environment Detection Settings",
+					},
+				},
+			},
 			//ENUMERATES
-			{"should create a valid AMT_EnvironmentDetectionSettingData Enumerate wsman message", "AMT_EnvironmentDetectionSettingData", wsmantesting.ENUMERATE, wsmantesting.ENUMERATE_BODY, elementUnderTest.Enumerate},
-			//PULLS
-			{"should create a valid AMT_EnvironmentDetectionSettingData Pull wsman message", "AMT_EnvironmentDetectionSettingData", wsmantesting.PULL, wsmantesting.PULL_BODY, func() string { return elementUnderTest.Pull(wsmantesting.EnumerationContext) }},
+			{
+				"should create a valid AMT_EnvironmentDetectionSettingData Enumerate wsman message", 
+				"AMT_EnvironmentDetectionSettingData", 
+				wsmantesting.ENUMERATE, 
+				wsmantesting.ENUMERATE_BODY, 
+				func() (Response, error) {
+					//client.CurrentMessage = "Enumerate"
+					return elementUnderTest.Enumerate()
+				}, 
+				Body{
+					XMLName: xml.Name{Space: "http://www.w3.org/2003/05/soap-envelope", Local: "Body"},
+					EnumerateResponse: common.EnumerateResponse{
+						EnumerationContext: "61000000-0000-0000-0000-000000000000",
+					},
+				},
+			},
+			// //PULLS
+			// {"should create a valid AMT_EnvironmentDetectionSettingData Pull wsman message", "AMT_EnvironmentDetectionSettingData", wsmantesting.PULL, wsmantesting.PULL_BODY, func() string { return elementUnderTest.Pull(wsmantesting.EnumerationContext) }},
 		}
-
 		for _, test := range tests {
 			t.Run(test.name, func(t *testing.T) {
-				correctResponse := wsmantesting.ExpectedResponse(messageID, resourceUriBase, test.method, test.action, "", test.body)
+				expectedXMLInput := wsmantesting.ExpectedResponse(messageID, resourceUriBase, test.method, test.action, "", test.body)
 				messageID++
-				response := test.responseFunc()
-				if response != correctResponse {
-					assert.Equal(t, correctResponse, response)
-				}
+				response, err := test.responseFunc()
+				println(response.XMLOutput)
+				assert.NoError(t, err)
+				assert.Equal(t, expectedXMLInput, response.XMLInput)
+				assert.Equal(t, test.expectedResponse, response.Body)
 			})
 		}
 	})
