baa8d6dece70b14e2ead8f2647f0dda0122d7547
Zaid <zaid.usmani@intel.com>
2023-10-11 00:15:48 -0700

    feat(amt): add execute support for userinitiated

--- pkg/amt/userinitiatedconnection/service.go
+++ pkg/amt/userinitiatedconnection/service.go
@@ -6,15 +6,38 @@
 package userinitiatedconnection
 
 import (
+	"encoding/json"
+	"encoding/xml"
+
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/common"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/message"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/amt/actions"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/amt/redirection"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsman"
 )
 
 type RequestedState int
 
 const AMT_UserInitiatedConnectionService = "AMT_UserInitiatedConnectionService"
 
+type (
+	Response struct {
+		*wsman.Message
+		XMLName xml.Name 		`xml:"Envelope"`
+		Header  message.Header  `xml:"Header"`
+		Body	Body			`xml:"Body"`
+	}
+	Body struct {
+		XMLName           		  xml.Name          	  `xml:"Body"`
+		User	  				  User  				  `xml:"AMT_AuthorizationService"`
+		
+		EnumerateResponse 		  common.EnumerateResponse
+		
+	}
+	User struct {
+
+	}
+)
 const (
 	AllInterfacesDisabled      RequestedState = 32768
 	BIOSInterfaceEnabled       RequestedState = 32769
@@ -22,8 +45,17 @@ const (
 	BIOSandOSInterfacesEnabled RequestedState = 32771
 )
 
+func (w *Response) JSON() string {
+	jsonOutput, err := json.Marshal(w.Body)
+	if err != nil {
+		return ""
+	}
+	return string(jsonOutput)
+}
+
 type Service struct {
 	base message.Base
+	client wsman.WSManClient
 }
 
 func NewUserInitiatedConnectionService(wsmanMessageCreator *message.WSManMessageCreator) Service {
@@ -32,14 +64,56 @@ func NewUserInitiatedConnectionService(wsmanMessageCreator *message.WSManMessage
 	}
 }
 
+func NewUserInitiatedConnectionServiceWithClient(wsmanMessageCreator *message.WSManMessageCreator, client wsman.WSManClient) Service {
+	return Service{
+		base:   message.NewBaseWithClient(wsmanMessageCreator, AMT_UserInitiatedConnectionService, client),
+		client: client,
+	}
+}
 // Get retrieves the representation of the instance
-func (UserInitiatedConnectionService Service) Get() string {
-	return UserInitiatedConnectionService.base.Get(nil)
+func (UserInitiatedConnectionService Service) Get() (response Response, err error) {
+
+	response = Response{
+		Message: &wsman.Message{
+			XMLInput: UserInitiatedConnectionService.base.Get(nil),
+		},
+	}
+
+	// send the message to AMT
+	err = UserInitiatedConnectionService.base.Execute(response.Message)
+	if err != nil {
+		return
+	}
+
+	// put the xml response into the go struct
+	err = xml.Unmarshal([]byte(response.XMLOutput), &response)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
 // Enumerates the instances of this class
-func (UserInitiatedConnectionService Service) Enumerate() string {
-	return UserInitiatedConnectionService.base.Enumerate()
+func (UserInitiatedConnectionService Service) Enumerate() (response Response, err error) {
+	response = Response{
+		Message: &wsman.Message{
+			XMLInput: UserInitiatedConnectionService.base.Enumerate(),
+		},
+	}
+	// send the message to AMT
+	err = UserInitiatedConnectionService.base.Execute(response.Message)
+	if err != nil {
+		return
+	}
+
+	// put the xml response into the go struct
+	err = xml.Unmarshal([]byte(response.XMLOutput), &response)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
 // Pulls instances of this class, following an Enumerate operation
--- pkg/amt/userinitiatedconnection/service_test.go
+++ pkg/amt/userinitiatedconnection/service_test.go
@@ -6,19 +6,26 @@
 package userinitiatedconnection
 
 import (
+	"encoding/xml"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/message"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsmantesting"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsman"
+	//"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/common"
 )
 
 func TestAMT_UserInitiatedConnectionService(t *testing.T) {
 	messageID := 0
 	resourceUriBase := "http://intel.com/wbem/wscim/1/amt-schema/1/"
 	wsmanMessageCreator := message.NewWSManMessageCreator(resourceUriBase)
-	elementUnderTest := NewUserInitiatedConnectionService(wsmanMessageCreator)
+	//client := MockClient{} // wsman.NewClient("http://localhost:16992/wsman", "admin", "P@ssw0rd", true)
+	//elementUnderTest := NewServiceWithClient(wsmanMessageCreator, &client)
+	// enumerationId := ""
+	client := wsman.NewClient("http://localhost:16992/wsman", "admin", "Intel123!", true)
+	elementUnderTest := NewUserInitiatedConnectionServiceWithClient(wsmanMessageCreator, client)
 
 	t.Run("amt_* Tests", func(t *testing.T) {
 		tests := []struct {
@@ -26,24 +33,56 @@ func TestAMT_UserInitiatedConnectionService(t *testing.T) {
 			method       string
 			action       string
 			body         string
-			responseFunc func() string
+			responseFunc     func() (Response, error)
+			expectedResponse interface{}
 		}{
 			//GETS
-			{"should create a valid AMT_UserInitiatedConnectionService Get wsman message", "AMT_UserInitiatedConnectionService", wsmantesting.GET, "", elementUnderTest.Get},
+			{
+				"should create a valid AMT_UserInitiatedConnectionService Get wsman message", 
+				"AMT_UserInitiatedConnectionService", 
+				wsmantesting.GET, 
+				"", 
+				func() (Response, error) {
+					//currentMessage = "Get"
+					return elementUnderTest.Get()
+				},
+				Body{
+					XMLName: xml.Name{Space: "http://www.w3.org/2003/05/soap-envelope", Local: "Body"},
+					User: User{
+						
+					},
+				},
+			},
 			//ENUMERATES
-			{"should create a valid AMT_UserInitiatedConnectionService Enumerate wsman message", "AMT_UserInitiatedConnectionService", wsmantesting.ENUMERATE, wsmantesting.ENUMERATE_BODY, elementUnderTest.Enumerate},
+			// {
+			// 	"should create a valid AMT_UserInitiatedConnectionService Enumerate wsman message", 
+			// 	"AMT_UserInitiatedConnectionService", 
+			// 	wsmantesting.ENUMERATE, 
+			// 	wsmantesting.ENUMERATE_BODY, 
+			// 	func() (Response, error) {
+			// 		//client.CurrentMessage = "Enumerate"
+			// 		return elementUnderTest.Enumerate()
+			// 	}, 
+			// 	Body{
+			// 		XMLName: xml.Name{Space: "", Local: ""},
+			// 		EnumerateResponse: common.EnumerateResponse{
+			// 			EnumerationContext: "",
+			// 		},
+			// 	},
+			// },
 			//PULLS
-			{"should create a valid AMT_UserInitiatedConnectionService Pull wsman message", "AMT_UserInitiatedConnectionService", wsmantesting.PULL, wsmantesting.PULL_BODY, func() string { return elementUnderTest.Pull(wsmantesting.EnumerationContext) }},
+			//{"should create a valid AMT_UserInitiatedConnectionService Pull wsman message", "AMT_UserInitiatedConnectionService", wsmantesting.PULL, wsmantesting.PULL_BODY, func() string { return elementUnderTest.Pull(wsmantesting.EnumerationContext) }},
 		}
 
 		for _, test := range tests {
 			t.Run(test.name, func(t *testing.T) {
-				correctResponse := wsmantesting.ExpectedResponse(messageID, resourceUriBase, test.method, test.action, "", test.body)
+				expectedXMLInput := wsmantesting.ExpectedResponse(messageID, resourceUriBase, test.method, test.action, "", test.body)
 				messageID++
-				response := test.responseFunc()
-				if response != correctResponse {
-					assert.Equal(t, correctResponse, response)
-				}
+				response, err := test.responseFunc()
+				println(response.XMLOutput)
+				assert.NoError(t, err)
+				assert.Equal(t, expectedXMLInput, response.XMLInput)
+				assert.Equal(t, test.expectedResponse, response.Body)
 			})
 		}
 	})
