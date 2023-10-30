c3a1f3c2a2ee14c24bdfa08751efdbe889067bda
Zaid <zaid.usmani@intel.com>
2023-10-30 14:58:28 -0700

    feat(amt): adds execute support for TLS

--- pkg/amt/tls/settingdata.go
+++ pkg/amt/tls/settingdata.go
@@ -6,12 +6,38 @@
 package tls
 
 import (
+	"encoding/json"
+	"encoding/xml"
+
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/message"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/cim/models"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/common"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsman"
+)
+
+type (
+	Response struct {
+		*wsman.Message
+		XMLName xml.Name       `xml:"Envelope"`
+		Header  message.Header `xml:"Header"`
+		Body    Body           `xml:"Body"`
+	}
+	Body struct {
+		XMLName    xml.Name   `xml:"Body"`
+		TlsSetting TlsSetting `xml:"AMT_TLSSettingData"`
+
+		EnumerateResponse common.EnumerateResponse
+	}
+	TlsSetting struct {
+		AcceptNonSecureConnections bool
+		ElementName                string
+		Enabled                    bool
+		InstanceID                 string
+		MutualAuthentication       bool
+	}
 )
 
 const AMT_TLSSettingData = "AMT_TLSSettingData"
-
 type TLSSettingData struct {
 	models.SettingData
 	MutualAuthentication          bool
@@ -20,8 +46,25 @@ type TLSSettingData struct {
 	AcceptNonSecureConnections    bool
 	NonSecureConnectionsSupported bool
 }
+
+func (w *Response) JSON() string {
+	jsonOutput, err := json.Marshal(w.Body)
+	if err != nil {
+		return ""
+	}
+	return string(jsonOutput)
+}
+
 type SettingData struct {
-	base message.Base
+	base   message.Base
+	client wsman.WSManClient
+}
+
+func NewTLSSettingDataWithClient(wsmanMessageCreator *message.WSManMessageCreator, client wsman.WSManClient) SettingData {
+	return SettingData{
+		base:   message.NewBaseWithClient(wsmanMessageCreator, AMT_TLSSettingData, client),
+		client: client,
+	}
 }
 
 func NewTLSSettingData(wsmanMessageCreator *message.WSManMessageCreator) SettingData {
@@ -31,21 +74,56 @@ func NewTLSSettingData(wsmanMessageCreator *message.WSManMessageCreator) Setting
 }
 
 // Get retrieves the representation of the instance
-func (TLSSettingData SettingData) Get() string {
-	return TLSSettingData.base.Get(nil)
+func (TLSSettingData SettingData) Get() (response Response, err error) {
+	response = Response{
+		Message: &wsman.Message{
+			XMLInput: TLSSettingData.base.Get(nil),
+		},
+	}
+
+	// send the message to AMT
+	err = TLSSettingData.base.Execute(response.Message)
+	if err != nil {
+		return
+	}
+
+	// put the xml response into the go struct
+	err = xml.Unmarshal([]byte(response.XMLOutput), &response)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
 // Enumerates the instances of this class
-func (TLSSettingData SettingData) Enumerate() string {
-	return TLSSettingData.base.Enumerate()
+func (TLSSettingData SettingData) Enumerate() (response Response, err error) {
+	response = Response{
+		Message: &wsman.Message{
+			XMLInput: TLSSettingData.base.Enumerate(),
+		},
+	}
+	// send the message to AMT
+	err = TLSSettingData.base.Execute(response.Message)
+	if err != nil {
+		return
+	}
+
+	// put the xml response into the go struct
+	err = xml.Unmarshal([]byte(response.XMLOutput), &response)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
 // Pulls instances of this class, following an Enumerate operation
-func (TLSSettingData SettingData) Pull(enumerationContext string) string {
-	return TLSSettingData.base.Pull(enumerationContext)
-}
+// func (TLSSettingData SettingData) Pull(enumerationContext string) string {
+// 	return TLSSettingData.base.Pull(enumerationContext)
+// }
 
 // Put will change properties of the selected instance
-func (TLSSettingData SettingData) Put(tlsSettingData TLSSettingData) string {
-	return TLSSettingData.base.Put(tlsSettingData, false, nil)
-}
+// func (TLSSettingData SettingData) Put(tlsSettingData TLSSettingData) string {
+// 	return TLSSettingData.base.Put(tlsSettingData, false, nil)
+// }
--- pkg/amt/tls/settingdata_test.go
+++ pkg/amt/tls/settingdata_test.go
@@ -6,44 +6,115 @@
 package tls
 
 import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/internal/message"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/common"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsmantesting"
+	"github.com/stretchr/testify/assert"
 )
 
+type MockClient struct {
+}
+
+const (
+	EnvelopeResponse = `<a:Envelope xmlns:a="http://www.w3.org/2003/05/soap-envelope" x-mlns:b="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:c="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd" xmlns:d="http://schemas.xmlsoap.org/ws/2005/02/trust" xmlns:e="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:f="http://schemas.dmtf.org/wbem/wsman/1/cimbinding.xsd" xmlns:g="http://intel.com/wbem/wscim/1/amt-schema/1/AMT_TLSSettingData" xmlns:h="http://schemas.dmtf.org/wbem/wscim/1/common" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><a:Header><b:To>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</b:To><b:RelatesTo>0</b:RelatesTo><b:Action a:mustUnderstand="true">`
+	GetBody          = `<g:AMT_TLSSettingData><g:CreationClassName>AMT_TLSSettingData</g:CreationClassName><g:ElementName>Intel(r) TLS Setting Data</g:ElementName><g:Name>Intel(r) AMT TLS Setting Data</g:Name><g:SystemCreationClassName>CIM_ComputerSystem</g:SystemCreationClassName><g:SystemName>ManagedSystem</g:SystemName></g:AMT_TLSSettingData>`
+)
+
+var currentMessage = ""
+
+func (c *MockClient) Post(msg string) ([]byte, error) {
+	// read an xml file from disk:
+	xmlFile, err := os.Open("../../wsmantesting/responses/amt/tls/settingdata/" + strings.ToLower(currentMessage) + ".xml")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil, err
+	}
+	defer xmlFile.Close()
+	// read file into string
+	xmlData, err := io.ReadAll(xmlFile)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, err
+	}
+	// strip carriage returns and new line characters
+	xmlData = []byte(strings.ReplaceAll(string(xmlData), "\r\n", ""))
+
+	// Simulate a successful response for testing.
+	return []byte(xmlData), nil
+}
 func TestAMT_TLSSettingData(t *testing.T) {
 	messageID := 0
 	resourceUriBase := "http://intel.com/wbem/wscim/1/amt-schema/1/"
 	wsmanMessageCreator := message.NewWSManMessageCreator(resourceUriBase)
-	elementUnderTest := NewTLSSettingData(wsmanMessageCreator)
+	client := MockClient{} 
+	elementUnderTest := NewTLSSettingDataWithClient(wsmanMessageCreator, &client)
 
 	t.Run("amt_* Tests", func(t *testing.T) {
 		tests := []struct {
-			name         string
-			method       string
-			action       string
-			body         string
-			responseFunc func() string
+			name             string
+			method           string
+			action           string
+			body             string
+			responseFunc     func() (Response, error)
+			expectedResponse interface{}
 		}{
 			//GETS
-			{"should create a valid AMT_TLSSettingData Get wsman message", "AMT_TLSSettingData", wsmantesting.GET, "", elementUnderTest.Get},
+			{
+				"should create a valid AMT_TLSSettingData Get wsman message",
+				"AMT_TLSSettingData",
+				wsmantesting.GET,
+				"",
+				func() (Response, error) {
+					currentMessage = "Get"
+					return elementUnderTest.Get()
+				},
+				Body{
+					XMLName: xml.Name{Space: "http://www.w3.org/2003/05/soap-envelope", Local: "Body"},
+					TlsSetting: TlsSetting{
+						AcceptNonSecureConnections: false,
+						ElementName:                "Intel(r) AMT 802.3 TLS Settings",
+						Enabled:                    false,
+						InstanceID:                 "Intel(r) AMT 802.3 TLS Settings",
+						MutualAuthentication:       false,
+					},
+				},
+			},
+
 			//ENUMERATES
-			{"should create a valid AMT_TLSSettingData Enumerate wsman message", "AMT_TLSSettingData", wsmantesting.ENUMERATE, wsmantesting.ENUMERATE_BODY, elementUnderTest.Enumerate},
+			{"should create a valid AMT_TLSSettingData Enumerate wsman message",
+				"AMT_TLSSettingData",
+				wsmantesting.ENUMERATE,
+				wsmantesting.ENUMERATE_BODY,
+				func() (Response, error) {
+					currentMessage = "Enumerate"
+					return elementUnderTest.Enumerate()
+				},
+				Body{
+					XMLName: xml.Name{Space: "http://www.w3.org/2003/05/soap-envelope", Local: "Body"},
+					EnumerateResponse: common.EnumerateResponse{
+						EnumerationContext: "CA000000-0000-0000-0000-000000000000",
+					},
+				},
+			},
 			//PULLS
-			{"should create a valid AMT_TLSSettingData Pull wsman message", "AMT_TLSSettingData", wsmantesting.PULL, wsmantesting.PULL_BODY, func() string { return elementUnderTest.Pull(wsmantesting.EnumerationContext) }},
+			//{"should create a valid AMT_TLSSettingData Pull wsman message", "AMT_TLSSettingData", wsmantesting.PULL, wsmantesting.PULL_BODY, func() string { return elementUnderTest.Pull(wsmantesting.EnumerationContext) }},
 		}
 
 		for _, test := range tests {
 			t.Run(test.name, func(t *testing.T) {
-				correctResponse := wsmantesting.ExpectedResponse(messageID, resourceUriBase, test.method, test.action, "", test.body)
+				expectedXMLInput := wsmantesting.ExpectedResponse(messageID, resourceUriBase, test.method, test.action, "", test.body)
 				messageID++
-				response := test.responseFunc()
-				if response != correctResponse {
-					assert.Equal(t, correctResponse, response)
-				}
+				response, err := test.responseFunc()
+				assert.NoError(t, err)
+				assert.Equal(t, expectedXMLInput, response.XMLInput)
+				assert.Equal(t, test.expectedResponse, response.Body)
 			})
 		}
 	})
