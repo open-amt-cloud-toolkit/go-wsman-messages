006eeb13c9b953938fcd5884bdc931dd7afe0727
Zaid <zaid.usmani@intel.com>
2023-10-30 16:25:44 -0700

    feat(amt): adds execute support for management

--- pkg/amt/managementpresence/remotesap.go
+++ pkg/amt/managementpresence/remotesap.go
@@ -60,7 +60,6 @@ func NewManagementPresenceRemoteSAP(wsmanMessageCreator *message.WSManMessageCre
 
 // Get retrieves the representation of the instance
 func (ManagementPresenceRemoteSAP RemoteSAP) Get() (response Response, err error) {
-
 	response = Response{
 		Message: &wsman.Message{
 			XMLInput: ManagementPresenceRemoteSAP.base.Get(nil),
@@ -84,7 +83,6 @@ func (ManagementPresenceRemoteSAP RemoteSAP) Get() (response Response, err error
 
 // Enumerates the instances of this class
 func (ManagementPresenceRemoteSAP RemoteSAP) Enumerate() (response Response, err error) {
-
 	response = Response{
 		Message: &wsman.Message{
 			XMLInput: ManagementPresenceRemoteSAP.base.Enumerate(),
@@ -106,12 +104,12 @@ func (ManagementPresenceRemoteSAP RemoteSAP) Enumerate() (response Response, err
 }
 
 // Pulls instances of this class, following an Enumerate operation
-func (ManagementPresenceRemoteSAP RemoteSAP) Pull(enumerationContext string) string {
-	return ManagementPresenceRemoteSAP.base.Pull(enumerationContext)
-}
+// func (ManagementPresenceRemoteSAP RemoteSAP) Pull(enumerationContext string) string {
+// 	return ManagementPresenceRemoteSAP.base.Pull(enumerationContext)
+// }
 
-// Delete removes a the specified instance
-func (ManagementPresenceRemoteSAP RemoteSAP) Delete(handle string) string {
-	selector := message.Selector{Name: "Name", Value: handle}
-	return ManagementPresenceRemoteSAP.base.Delete(selector)
-}
+// // Delete removes a the specified instance
+// func (ManagementPresenceRemoteSAP RemoteSAP) Delete(handle string) string {
+// 	selector := message.Selector{Name: "Name", Value: handle}
+// 	return ManagementPresenceRemoteSAP.base.Delete(selector)
+// }
