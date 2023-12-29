ea951c81efd1d71c1a0b9afff291cf2df13efb8a
Tim Shockley <timothy.shockley@intel.com>
2023-12-29 16:09:51 -0700

    feat(amt): added responses for timesynchronization

--- pkg/amt/timesynchronization/service.go
+++ pkg/amt/timesynchronization/service.go
@@ -14,6 +14,22 @@ import (
 
 const AMT_TimeSynchronizationService = "AMT_TimeSynchronizationService"
 
+type (
+	Response struct {
+		XMLName xml.Name `xml:"Envelope"`
+		Header  message.Header
+		Body
+	}
+	Body struct {
+		GetLowAccuracyTimeSynch_OUTPUT
+		SetHighAccuracyTimeSynch_OUTPUT message.ReturnValue
+	}
+	GetLowAccuracyTimeSynch_OUTPUT struct {
+		Ta0         int64
+		ReturnValue int
+	}
+)
+
 type Service struct {
 	base message.Base
 }
@@ -32,31 +48,31 @@ func NewTimeSynchronizationService(wsmanMessageCreator *message.WSManMessageCrea
 }
 
 // Get retrieves the representation of the instance
-func (TimeSynchronizationService Service) Get() string {
-	return TimeSynchronizationService.base.Get(nil)
+func (s Service) Get() string {
+	return s.base.Get(nil)
 }
 
 // Enumerates the instances of this class
-func (TimeSynchronizationService Service) Enumerate() string {
-	return TimeSynchronizationService.base.Enumerate()
+func (s Service) Enumerate() string {
+	return s.base.Enumerate()
 }
 
 // Pulls instances of this class, following an Enumerate operation
-func (TimeSynchronizationService Service) Pull(enumerationContext string) string {
-	return TimeSynchronizationService.base.Pull(enumerationContext)
+func (s Service) Pull(enumerationContext string) string {
+	return s.base.Pull(enumerationContext)
 }
-func (TLSCredentialContext Service) SetHighAccuracyTimeSynch(ta0, tm1, tm2 int64) string {
-	header := TLSCredentialContext.base.WSManMessageCreator.CreateHeader(string(actions.SetHighAccuracyTimeSynch), AMT_TimeSynchronizationService, nil, "", "")
-	body := TLSCredentialContext.base.WSManMessageCreator.CreateBody("SetHighAccuracyTimeSynch_INPUT", AMT_TimeSynchronizationService, &SetHighAccuracyTimeSynch_INPUT{
+func (s Service) SetHighAccuracyTimeSynch(ta0, tm1, tm2 int64) string {
+	header := s.base.WSManMessageCreator.CreateHeader(string(actions.SetHighAccuracyTimeSynch), AMT_TimeSynchronizationService, nil, "", "")
+	body := s.base.WSManMessageCreator.CreateBody("SetHighAccuracyTimeSynch_INPUT", AMT_TimeSynchronizationService, &SetHighAccuracyTimeSynch_INPUT{
 		Ta0: ta0,
 		Tm1: tm1,
 		Tm2: tm2,
 	})
-	return TLSCredentialContext.base.WSManMessageCreator.CreateXML(header, body)
+	return s.base.WSManMessageCreator.CreateXML(header, body)
 }
 
-func (TLSCredentialContext Service) GetLowAccuracyTimeSynch() string {
-	header := TLSCredentialContext.base.WSManMessageCreator.CreateHeader(string(actions.GetLowAccuracyTimeSynch), AMT_TimeSynchronizationService, nil, "", "")
-	body := TLSCredentialContext.base.WSManMessageCreator.CreateBody("GetLowAccuracyTimeSynch_INPUT", AMT_TimeSynchronizationService, nil)
-	return TLSCredentialContext.base.WSManMessageCreator.CreateXML(header, body)
+func (s Service) GetLowAccuracyTimeSynch() string {
+	header := s.base.WSManMessageCreator.CreateHeader(string(actions.GetLowAccuracyTimeSynch), AMT_TimeSynchronizationService, nil, "", "")
+	body := s.base.WSManMessageCreator.CreateBody("GetLowAccuracyTimeSynch_INPUT", AMT_TimeSynchronizationService, nil)
+	return s.base.WSManMessageCreator.CreateXML(header, body)
 }
