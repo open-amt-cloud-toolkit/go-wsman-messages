c58aebcfe12b8a4e23cecd81ffa97005579ae902
Matt Primrose <47540908+matt-primrose@users.noreply.github.com>
2024-01-25 16:38:01 -0700

    docs(cim): adds code comments for actions and models (#184)

--- pkg/wsman/cim/models/methods.go
+++ pkg/wsman/cim/models/methods.go
@@ -3,8 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  **********************************************************************/
 
+// Package models provides a set of utility types, constants, and functions that are used broadly across amt, cim, and ips packages
 package models
 
+// HasSelector checks the SelectorSet and returns true if the SelectorSet contains a Selector
 func (rp *ReferenceParmetersNoNamespace) HasSelector(name string, value string) bool {
 	for _, selector := range rp.SelectorSet {
 		if selector.Name == name && selector.Value == value {
@@ -14,6 +16,7 @@ func (rp *ReferenceParmetersNoNamespace) HasSelector(name string, value string) 
 	return false
 }
 
+// GetSelectorValue returns the Value property of a selector found in a SelectorSet based on the selector name
 func (rp *ReferenceParmetersNoNamespace) GetSelectorValue(name string) string {
 	for _, selector := range rp.SelectorSet {
 		if selector.Name == name {
