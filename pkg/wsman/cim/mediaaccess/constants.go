4b6f34db85e7b5c77bb436cb0f3b2049e3d0ed02
zaidusmani26 <106174728+zaidusmani26@users.noreply.github.com>
2024-02-12 11:31:48 -0700

    docs(cim): adds code comments for mediaaccess package (#220)

--- pkg/wsman/cim/mediaaccess/constants.go
+++ pkg/wsman/cim/mediaaccess/constants.go
@@ -24,6 +24,75 @@ const (
 )
 
 const (
+	EnabledDefaultEnabled           EnabledDefault = 2
+	EnabledDefaultDisabled          EnabledDefault = 3
+	EnabledDefaultNotApplicable     EnabledDefault = 5
+	EnabledDefaultEnabledbutOffline EnabledDefault = 6
+	EnabledDefaultNoDefault         EnabledDefault = 7
+	EnabledDefaultQuiesce           EnabledDefault = 9
+	EnabledDefaultDMTFReserved      EnabledDefault = 32768
+	EnabledDefaultVendorReserved    EnabledDefault = 65535
+)
+
+const (
+	EnabledStateUnknown EnabledState = iota
+	EnabledStateOther
+	EnabledStateEnabled
+	EnabledStateDisabled
+	EnabledStateShuttingDown
+	EnabledStateNotApplicable
+	EnabledStateEnabledbutOffline
+	EnabledStateInTest
+	EnabledStateDeferred
+	EnabledStateQuiesce
+	EnabledStateStarting
+	EnabledStateDMTFReserved
+	EnabledStateVendorReserved
+)
+
+const (
+	OperationalStatusUnknown OperationalStatus = iota
+	OperationalStatusOther
+	OperationalStatusOK
+	OperationalStatusDegraded
+	OperationalStatusStressed
+	OperationalStatusPredictiveFailure
+	OperationalStatusError
+	OperationalStatusNonRecoverableError
+	OperationalStatusStarting
+	OperationalStatusStopping
+	OperationalStatusStopped
+	OperationalStatusInService
+	OperationalStatusNoContact
+	OperationalStatusLostCommunication
+	OperationalStatusAborted
+	OperationalStatusDormant
+	OperationalStatusSupportingEntityinError
+	OperationalStatusCompleted
+	OperationalStatusPowerMode
+	OperationalStatusRelocating
+	OperationalStatusDMTFReserved
+	OperationalStatusVendorReserved
+)
+
+const (
+	RequestedStateEnabled RequestedState = iota + 2
+	RequestedStateDisabled
+	RequestedStateShutDown
+	RequestedStateNoChange
+	RequestedStateOffline
+	RequestedStateTest
+	RequestedStateDeferred
+	RequestedStateQuiesce
+	RequestedStateReboot
+	RequestedStateReset
+	RequestedStateNotApplicable
+	RequestedStateDMTFReserved
+	RequestedStateVendorReserved
+	RequestedStateUnknown RequestedState = 0
+)
+
+const (
 	SecurityValuesOther SecurityValues = iota + 1
 	SecurityValuesUnknown
 	SecurityValuesNone
