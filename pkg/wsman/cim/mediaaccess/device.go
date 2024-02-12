b7b5971bc8c4e818c5980065b6782fe000ce2d9c
Matt Primrose <47540908+matt-primrose@users.noreply.github.com>
2024-02-12 12:23:26 -0700

    revert: "docs(cim): adds code comments for mediaaccess package (#220)"
    This reverts commit 4b6f34db85e7b5c77bb436cb0f3b2049e3d0ed02.

--- pkg/wsman/cim/mediaaccess/device.go
+++ pkg/wsman/cim/mediaaccess/device.go
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  **********************************************************************/
 
-// Package bios facilitiates communication with Intel® AMT devices to represent the ability to access one or more media and use this media to store and retrieve data.
 package mediaaccess
 
 import (
