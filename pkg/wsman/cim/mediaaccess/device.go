2e4a5ea73f95f025fcac6406cc679f1834faa4d7
zaidusmani26 <106174728+zaidusmani26@users.noreply.github.com>
2024-02-13 09:24:39 -0700

    docs(cim): adds code comments for mediaaccess package (#225)
    * docs(cim): adds code comments for mediaaccess package
    
    * revert: "docs(cim): adds code comments for mediaaccess package"
    
    * docs(cim): fixing typo and removing constants

--- pkg/wsman/cim/mediaaccess/device.go
+++ pkg/wsman/cim/mediaaccess/device.go
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  **********************************************************************/
 
+// Package mediaaccess facilitiates communication with Intel® AMT devices to represent the ability to access one or more media and use this media to store and retrieve data.
 package mediaaccess
 
 import (
