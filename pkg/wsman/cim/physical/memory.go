1bd6181212905f93868f9f6b61aa241597963650
Zaid <zaid.usmani@intel.com>
2024-03-04 10:37:30 -0700

    docs(cim): adds code comments for physical package

--- pkg/wsman/cim/physical/memory.go
+++ pkg/wsman/cim/physical/memory.go
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  **********************************************************************/
 
-// Package physical facilitiates communication with Intel® AMT devices as PhysicalMemory is a subclass of CIM_Chip, representing low level memory devices - SIMMS, DIMMs, raw memory chips, etc.
+// Package physical facilitates communications with Intel® AMT devices to get the PhysicalMemory as a subclass of CIM_Chip, representing low level memory devices - SIMMS, DIMMs, raw memory chips, etc.
 package physical
 
 import (
