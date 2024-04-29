1908c56c0f537b39d3474d7ff9596fd59f88cd8c
Matt Primrose <matt.c.primrose@intel.com>
2024-04-29 08:06:42 -0700

    feat(cim): adds setbootconfigrole function

--- pkg/wsman/cim/boot/service.go
+++ pkg/wsman/cim/boot/service.go
@@ -7,8 +7,11 @@ package boot
 
 import (
 	"encoding/xml"
+	"strconv"
+	"strings"
 
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/v2/internal/message"
+	"github.com/open-amt-cloud-toolkit/go-wsman-messages/v2/pkg/wsman/cim/methods"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/v2/pkg/wsman/client"
 )
 
@@ -65,6 +68,36 @@ func (service Service) Pull(enumerationContext string) (response Response, err e
 	response = Response{
 		Message: &client.Message{
 			XMLInput: service.base.Pull(enumerationContext),
+		},
+	}
+	err = service.base.Execute(response.Message)
+	if err != nil {
+		return
+	}
+	err = xml.Unmarshal([]byte(response.XMLOutput), &response)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (service Service) SetBootConfigRole(instanceID string, role int) (response Response, err error) {
+	header := service.base.WSManMessageCreator.CreateHeader(methods.GenerateAction(CIM_BootService, SetBootConfigRole), CIM_BootService, nil, "", "")
+
+	var body strings.Builder
+	body.WriteString(`<Body><h:SetBootConfigRole_INPUT xmlns:h="`)
+	body.WriteString(service.base.WSManMessageCreator.ResourceURIBase)
+	body.WriteString(`CIM_BootService"><h:BootConfigSetting><Address xmlns="http://schemas.xmlsoap.org/ws/2004/08/addressing">http://schemas.xmlsoap.org/ws/2004/08/addressing</Address><ReferenceParameters xmlns="http://schemas.xmlsoap.org/ws/2004/08/addressing"><ResourceURI xmlns="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd">http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_BootConfigSetting</ResourceURI><SelectorSet xmlns="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd">`)
+	body.WriteString(`<Selector Name="InstanceID">`)
+	body.WriteString(instanceID)
+	body.WriteString(`</Selector></SelectorSet></ReferenceParameters></h:BootConfigSetting>`)
+	body.WriteString(`<h:Role>`)
+	body.WriteString(strconv.Itoa(role))
+	body.WriteString(`</h:Role></h:SetBootConfigRole_INPUT></Body>`)
+
+	response = Response{
+		Message: &client.Message{
+			XMLInput: service.base.WSManMessageCreator.CreateXML(header, body.String()),
 		},
 	}
 	err = service.base.Execute(response.Message)
